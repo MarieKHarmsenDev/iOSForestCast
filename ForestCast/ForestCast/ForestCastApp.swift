fdf8ec5b446cd3846cee1e1f43903be4f5b09947
Marie Harmsen <blazingbodylife@gmail.com>
2025-04-03 13:28:24 +0100

    Set up project

---
+++ ForestCast/ForestCast/ForestCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForestCastApp.swift
+//  ForestCast
+//
+//  Created by Marie Harmsen on 03/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ForestCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ ForestCast/ForestCastTests/ForestCastTests.swift
@@ -0,0 +1,16 @@
+//
+//  ForestCastTests.swift
+//  ForestCastTests
+//
+//  Created by Marie Harmsen on 03/04/2025.
+//
+
+import Testing
+
+struct ForestCastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
